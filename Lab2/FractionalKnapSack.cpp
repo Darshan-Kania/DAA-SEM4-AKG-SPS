31183c46b4d9ea953a7736733486cc4856bdf7a8
Darshan-Kania <darshankania.ddu0109@gmail.com>
2024-12-12 18:51:44 +0530

    Completed Fractional KnapSack

--- Lab2/FractionalKnapSack.cpp
+++ Lab2/FractionalKnapSack.cpp
@@ -1,10 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    /*Darshan Kania*/	
-    
+    /*Darshan Kania*/
+    int noItems;
+    cin >> noItems;
+    vector<pair<double, pair<int, int>>> items(noItems);
+    for (auto &it : items)
+    {
+        cout << "Enter Item Profit: ";
+        cin >> it.second.first;
+        cout << "Enter Item Weight: ";
+        cin >> it.second.second;
+        it.first = ((it.second.first) / (double(it.second.second)));
+    }
+    sort(items.begin(), items.end());
+    reverse(items.begin(), items.end());
+    int curWt = 0;
+    int capacity;
+    cout << "Capacity is: ";
+    cin >> capacity;
+    int i = 0;
+    int totalProfit = 0;
+    while (i < noItems && curWt < capacity)
+    {
+        if (curWt + items[i].second.second <= capacity)
+        {
+            curWt += items[i].second.second;
+            totalProfit += items[i].second.first;
+        }
+        else
+        {
+            double ratio = ((capacity - curWt) / (double(items[i].second.second)));
+            curWt += ratio * items[i].second.second;
+            totalProfit += ratio * items[i].second.first;
+        }
+        i++;
+    }
+    cout << totalProfit << endl;
     return 0;
 }
